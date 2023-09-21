526105c5a68ff73584e58c9a85b137c73fecfbbe
Raven <wu_bin3@dahuatech.com>
2023-09-21 19:33:17 +0800

    remove attrs on field

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,9 @@ use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Error};
 #[proc_macro_derive(SizeOfNoPadding)]
 pub fn derive_size_of_no_padding(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut input = parse_macro_input!(input as DeriveInput);
-    if let Data::Enum(e) = input.data {
-        return Error::new(e.enum_token.span, "SizeOfNoPadding not work for enum")
-            .into_compile_error()
-            .into();
+    if let Err(e) = remove_attrs(&mut input.data) {
+        return e.into_compile_error().into();
     }
-
     input.attrs.clear();
     input.attrs.push(parse_quote!(#[repr(packed)]));
     input.attrs.push(parse_quote!(#[allow(dead_code)]));
@@ -33,4 +30,25 @@ pub fn derive_size_of_no_padding(input: proc_macro::TokenStream) -> proc_macro::
         }
     }
     .into()
+}
+
+fn remove_attrs(data: &mut Data) -> Result<(), Error> {
+    match data {
+        Data::Struct(s) => {
+            match &mut s.fields {
+                syn::Fields::Named(n) => n.named.iter_mut().for_each(|f| f.attrs.clear()),
+                syn::Fields::Unnamed(u) => u.unnamed.iter_mut().for_each(|f| f.attrs.clear()),
+                _ => {}
+            }
+            Ok(())
+        }
+        Data::Union(u) => {
+            u.fields.named.iter_mut().for_each(|f| f.attrs.clear());
+            Ok(())
+        }
+        Data::Enum(e) => Err(Error::new(
+            e.enum_token.span,
+            "SizeOfNoPadding not work for enum",
+        )),
+    }
 }
